796225419c409d85c5d70f28ff7b12b282ee2381
Luca D'Onofrio <lucadonofrio097@gmail.com>
2022-06-03 17:11:07 -0300

    Agregado metodo de deserializacion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-
+    	SerializacionGuardado saves = new SerializacionGuardado();
+    	Usuario user = new Usuario();
+    	List<Usuario> lista = saves.deserializacion("test", user);
     }
 }
--- src/SerializacionGuardado.java
+++ src/SerializacionGuardado.java
@@ -1,4 +1,10 @@
+import java.io.IOException;
+import java.io.Reader;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gson.Gson;
 
@@ -9,10 +15,30 @@ public class SerializacionGuardado implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public <T> void serializacionGuardado(String saveName, T saveData)
+	
+	public <T> void serializacion(String saveName, T saveData)
 	{
-		Gson gson = new Gson();
-		String json = gson.toJson(saveData);
+		
+	}
+	
+	public <T> List<T> deserializacion(String saveName, T saveData)
+	{
+		List<T> data = new ArrayList<T>();
+		try {
+			Reader myReader = Files.newBufferedReader(Paths.get("src/"+saveName+".json"));
+			Gson gson = new Gson();
+			data.add((T)gson.fromJson(myReader,Object.class));
+			System.out.println(data);
+		}
+		catch(ClassCastException e)
+		{
+			System.out.println("cast error");
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return data;
 	}
 	
 }
