948d6ee1234f87b440d8ee1b2dcae94d8ae4303e
Luca D'Onofrio <lucadonofrio097@gmail.com>
2022-06-08 17:50:32 -0300

    creado enum para nombre de archivos

---
+++ src/nombreArchivos.java
@@ -0,0 +1,30 @@
+
+public enum nombreArchivos {
+	
+	ADMINISTRADORES("admins"),
+	PACIENTES("pacientes"),
+	PROFESIONALES("profesionales"),
+	TRATAMIENTOS("tratamientos"),
+	PLANES("planes"),
+	ENFERMEDADES("enfermedades"),
+	TAREASBASICAS("tareasBasicas"),
+	TAREASALFANUMERICAS("tareasAlfanumericas"),
+	TAREASSINO("tareasSiNo"),
+	TAREASNUMERICAS("tareasNumericas");
+	
+	private String name;
+	
+	nombreArchivos(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
