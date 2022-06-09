659231430adcf5f9a468f7ca93ddb2585d8e24e5
Belen Robledo <beeelrobledo@gmail.com>
2022-06-09 17:25:34 -0300

    Carga JSON File de Enfermedades

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
+        List<String> sintomas = new ArrayList<>();
+        sintomas.add("Fiebre");
+        sintomas.add("Tos");
+        sintomas.add("Dolor de garganta");
+        sintomas.add("Congestion nasal");
+        sintomas.add("Dolores corporales");
+        Enfermedad e = new Enfermedad(sintomas, "Gripe");
+
+        List<Enfermedad> le = new ArrayList<>();
+        le.add(e);
+        SerializacionGuardado.serializacion(nombreArchivos.ENFERMEDADES.getName(), le);
     }
 }
