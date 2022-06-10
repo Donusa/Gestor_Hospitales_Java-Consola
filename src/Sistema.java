b883991b80e88768e113f8e3d6d22ace83669de6
Luca D'Onofrio <lucadonofrio097@gmail.com>
2022-06-10 16:16:58 -0300

    Verificacion de identidad terminada

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Sistema extends Thread{
 	static List<Usuario> users = new ArrayList<>();
@@ -19,14 +20,48 @@ public class Sistema extends Thread{
 		
 	}
 
-	public void verificacionIdentidad()
+	public void verificacionIdentidad(Usuario logUser)
 	{
+		Scanner scan = new Scanner(System.in);
+		Boolean flag = false, nameChecker = false, passChecker = false;
+		String userName = "";
+		String userPass = "";
+		Usuario currentLog = new Usuario();
+		users.addAll(SerializacionGuardado.deserializacion(nombreArchivos.ADMINISTRADORES.getName(), new Administrador()));
+		users.addAll(SerializacionGuardado.deserializacion(nombreArchivos.PACIENTES.getName(), new Paciente()));
+		users.addAll(SerializacionGuardado.deserializacion(nombreArchivos.PROFESIONALES.getName(), new Profesional()));
 		
-	}
-	
-	public void dayChangeReset()
-	{
-
+		do
+		{
+			System.out.println("Ingrese nombre de usuario");
+			userName = scan.nextLine();
+			for(Usuario u : users)
+			{
+				if(userName.equalsIgnoreCase(u.getUserName()))
+				{	
+					nameChecker = true;
+					currentLog = u;
+				}
+			}
+			if(nameChecker)
+			{
+				System.out.println("Ingrese Contraseña");
+				userPass = scan.nextLine();
+				if(userPass.equals(currentLog.getPassword()));
+				{
+					flag = true;
+					passChecker = true;
+				}
+			}
+			if(!passChecker || !nameChecker)
+			{
+				System.out.println("Datos no validos");
+				nameChecker = false;
+				passChecker = false;
+			}
+		}while(flag == false);
+		
+		scan.close();
 	}
 
 	public static <T> List<T> mergeListas(List<T> lista, List<T> listaAgregada)
