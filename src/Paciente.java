8fbf51795c142823510ade753220ae4476e4e72e
Belen Robledo <beeelrobledo@gmail.com>
2022-06-10 16:33:33 -0300

    Carga archivos JSON

--- src/Paciente.java
+++ src/Paciente.java
@@ -8,13 +8,11 @@ public class Paciente extends Usuario implements Menu{
 	private List<Tratamiento> tratamientos = new ArrayList<>();
 
 	public Paciente() {
-		tratamientos = new ArrayList<>();
 	}
 
-	public Paciente(Enfermedad e, Profesional p, String userName,
-			String email, String password, String userDni, String userCel) {
+	public Paciente(Enfermedad e, String profesionalEncargado, String userName, String email, String password, String userDni, String userCel) {
 		super(userName,email,password,userDni,userCel);
-		tratamientos.add(new Tratamiento(p.getUserName(), e));
+		tratamientos.add(new Tratamiento(profesionalEncargado, e));
 	}
 
 	public void ingresoDatosDeControl()
