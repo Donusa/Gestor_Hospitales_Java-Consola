0b2643a924cab49c10bbc86df5ba4fd772488085
Luca D'Onofrio <lucadonofrio097@gmail.com>
2022-06-14 19:01:37 -0300

    Creados metodos nuevos y fixeados anteriores de paciente

--- src/Paciente.java
+++ src/Paciente.java
@@ -33,25 +33,81 @@ public class Paciente extends Usuario implements Menu{
 	//---Metodos--------------------------------------------------------------------------------------------------------
 	public void ingresoDatosDeControl()
 	{
-		crearUser(this);
-		System.out.println("Ingrese la enfermedad:");
-		this.tratamientos.get(0).getPlan().getEnfermedad().setName(ScannerSingleton.getInstance().nextLine());    //en el get el "0" habría que cambiarlo
-		
+		int choice = 0;
+		for(Tratamiento t :this.getTratamientos())
+		{
+			for(Tarea tarea : t.getPlan().getTasks())
+			{
+				if(!tarea.isTaskDone()) {
+					System.out.println(tarea.getTaskName());
+					System.out.println("Realizo la tarea?");
+					System.out.println("1. Si");
+					System.out.println("2. No");
+					choice = Integer.parseInt(ScannerSingleton.getInstance().nextLine());
+					if (choice == 1)
+						tarea.setTaskDone(true);
+					else
+						tarea.setTaskDone(false);
+				}
+				if(tarea instanceof TareaNumerica)
+				{
+					System.out.println("Ingrese valor numerico correspondiente a la tarea");
+					((TareaNumerica) tarea).setNumero(Integer.parseInt(ScannerSingleton.getInstance().nextLine()));
+				}
+				else if(tarea instanceof TareaAlfanumerica)
+				{
+					System.out.println("Ingrese la informacion adicional requerida");
+					((TareaAlfanumerica) tarea).setInfo(ScannerSingleton.getInstance().nextLine());
+				}
+			}
+		}
 	}
 	
-	
 
+	private void notificacionesTareasSinRealizar() {
+		Boolean flag = false;
+		System.out.println("Tareas sin realizar : ");
+		for(Tratamiento t :this.getTratamientos())
+		{
+			for(int i = 0 ; i < t.getPlan().getTasks().size() ; i++) {
+				if(!t.getPlan().getTasks().get(i).isTaskDone())
+				{
+					flag = true;
+					System.out.println(t.getPlan().getTasks().get(i).getTaskName());
+				}
+			}
+		}
+		if(!flag)
+		{
+			System.out.println("No tiene tareas sin realizar!");
+		}
+	}
+	
 	@Override
 	public void menu() {
-		int choice = 0;
-
+		int choice = -1;
+		notificacionesTareasSinRealizar();
 		do{
 			try{
 				System.out.println("1. Ingreso de Datos de Control.\n"
+								 + "2. Mostrar Tareas\n"
 						         + "0. Salir.\n");
 				choice = Integer.parseInt(ScannerSingleton.getInstance().nextLine());
-				if(choice == 1){
+				
+				switch(choice)
+				{
+				case 1:
 					ingresoDatosDeControl();
+					break;
+				case 2:
+					for(Tratamiento t : this.getTratamientos())
+					{
+						System.out.println(t);//printea los tratamientos con sus planes y tareas
+					}
+					break;
+				case 0: break;
+				default: System.out.println("Opcion invalida");
+				break;
 				}
 			}
 			catch(InputMismatchException e){
